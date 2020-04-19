967b31b05dd51ef2fc0f2af4d2106e37c24b095f
laventin85 <32778449+laventin85@users.noreply.github.com>
2020-04-19 18:54:57 +0200

    Create ossn.fr.php
    Add translation French file

---
+++ locale/ossn.fr.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright 2014-2019 SOFTLAB24 LIMITED
+ * @license   SOFTLAB24 LIMITED, COMMERCIAL LICENSE  https://www.softlab24.com/license/commercial-license-v1
+ * @link      https://www.softlab24.com/
+ */
+$fr = array(
+		'admin:menubuilder' => 'Menu Builder',
+		'menubuilder:settings' => "Menu Builder",
+		'menubuilder:title' => 'Titre',
+		'menubuilder:url' => 'URL du menu',
+		'menubuilder:next' => 'Suivant',
+		'menubuilder:selecticon' => 'Selectioner une Icône',
+		'menubuilder:save' => 'Enregister',
+		'menubuilder:name' => 'Selectioner Menu',
+		'menubuilder:submenu' => 'Selectectionner Sous-menu',
+		'menubuilder:main' => 'Principal',
+		'menubuilder:menuitem:added' => 'Un élément a été ajouté au menu!',
+		'menubuilder:menuitem:failed' => "L'élément ne peut pas être ajouté dans le menu",
+		'menubuilder:fillfields' => "Tous les champs sont requis !",
+		'menubuilder:admin:name' => 'Menu',
+		'menubuilder:submenu' => 'Sous-menu',
+		'menubuilder:delete' => 'Effacer',
+		'menubuilder:menuitem:delete:failed' => "Impossible de supprimer l'élément du menu",
+		'menubuilder:menuitem:deleted' => "L'élément a été supprimé du menu",
+		'menubuilder:add' => 'Ajouter un élément au menu',
+		'menubuilder:newtab' => 'Ouvrir dans un nouvel onglet',
+		'menubuilder:no' => 'Non',
+		'menubuilder:yes' => 'Oui',
+		'menubuilder:invalid:url' => 'Format URL invalide',
+		'menubuilder:footer' => 'Pied de page',
+		'menubuilder:topbardropdown' => 'Liste déroulante de la barre supérieure (côté droit)',
+		'menubuilder:topbar:dropdown' => 'Liste déroulante de la barre supérieure (côté droit)',
+		
+		'menubuilder:admin:sidemenu' => "Barre supérieure de l'administrateur (côté droit)",
+		'menubuilder:topbaradmin' => "Barre supérieure de l'administrateur (côté gauche)",
+		'menubuilder:topbar:admin' => "Barre supérieure de l'administrateur (côté droit)",
+		'menubuilder:submenu:groups' => 'Groupes',
+		'menubuilder:submenu:bpage' => "Page D'entreprise",
+		'menubuilder:submenu:polls' => 'Sondages',
+		'menubuilder:submenu:blogs' => 'Blogs',
+		'menubuilder:submenu:themes' => 'Thèmes',
+		'menubuilder:submenu:site:settings' => 'Paramètres du site',
+		'menubuilder:submenu:user:manager' => "Gestionnaire d'utilisateurs",
+		
+		'menubuilder:newsfeed' => 'Sidebar',
+		'menubuilder:submenu:onlytoplevel' => 'Principal',
+		'menubuilder:submenu:components' => 'Composants',
+		'menubuilder:submenu:configure' => 'Configurer',
+		'menubuilder:submenu:links' => 'Liens',
+);
+ossn_register_languages('fr', $fr); 
